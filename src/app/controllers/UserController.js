b76b427ba4d6e7bab6ab5a0d4334810f59a53454
lucascust <lucascustodio@hotmail.com.br>
2020-02-04 01:57:47 -0300

    sequelize init

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,16 @@
+import User from '../models/User';
+
+
+class UserController {
+  async store(req, res) {
+    const { firstName, lastName, email } = await User.create(req.body);
+
+    return res.json({
+      firstName,
+      lastName,
+      email,
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/app/models/User.js
@@ -0,0 +1,9 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {});
+  return User;
+};
