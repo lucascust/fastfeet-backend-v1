abc757969cb4f89f2595fb2710b2f63d09d6918b
lucascust <lucascustodio@hotmail.com.br>
2020-02-08 05:23:15 -0300

    add files back to gitingore

---
+++ src/database/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+
+// Models import
+import User from '../app/models/User';
+import Recipient from '../app/models/Recipient';
+
+import databaseConfig from '../config/database';
+
+const models = [User, Recipient];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  // Método para conexão com a base de dados e carregar os models
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    // Percorre todos os models e inicia todos com a conexão feita acima
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
